e0057d56db73513313ea1fa1f52730681b65e2c2
Quinlan Taylor <qtaylor@lcs.on.ca>
2024-01-18 11:29:15 -0500

    Initial Commit

---
+++ Enhanced-To-Do-List/Enhanced_To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enhanced_To_Do_ListApp.swift
+//  Enhanced-To-Do-List
+//
+//  Created by Quin Taylor on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct Enhanced_To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
